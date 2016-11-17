3f737fd76ea8657513fd64b6c4c2ee11225f6199
kawakubox <shigefumi.kawakubo@gmail.com>
2016-11-17 20:32:28 +0900

    Fix failed spac.

--- spec/controllers/auctions_controller_spec.rb
+++ spec/controllers/auctions_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe AuctionsController, type: :controller do
+RSpec.describe AuctionsController do
   let(:title) { FFaker::Movie.title }
-  describe :create do
+  let(:params) do
+    { auction: { exhibitor: 'hoge', title: title } }
+  end
+  describe '#create' do
     it 'raises ActiveRecord::RecordNotFound when unknown user' do
-      expect {
-      post '/auctions.json', { auction: { exhibitor: 'hoge', title: title } }
-      }.to raise_error ActiveRecord::RecordNotFound
-    end    
+      expect { post :create, params: params }.to raise_error ActiveRecord::RecordNotFound
+    end
   end
 end
