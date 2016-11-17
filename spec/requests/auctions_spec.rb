8c1935909d3f2c2f01943ecae09af328bbcb8ead
kawakubox <shigefumi.kawakubo@gmail.com>
2016-11-17 20:44:38 +0900

    Resolve DEPRECATED

--- spec/requests/auctions_spec.rb
+++ spec/requests/auctions_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Auctions Request' do
   let!(:exhibitor) { FactoryGirl.create(:user) }
   let(:title) { FFaker::Movie.title }
+  let(:params) do
+    { auction: { exhibitor: exhibitor.slack_id, title: title } }
+  end
   it 'returns :created' do
-    post '/auctions.json', auction: { exhibitor: exhibitor.slack_id, title: title }
+    post '/auctions.json', params: params
     expect(response).to have_http_status :created
   end
 end
