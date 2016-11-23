3e9d72d067ea84c39757aa563c4ca7baec0bdb1e
kawakubox <shigefumi.kawakubo@gmail.com>
2016-11-23 12:18:30 +0900

    Change primary key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
+  self.primary_key = :slack_id
   has_many :bids
 
   validates :slack_id, presence: true, uniqueness: true
--- db/migrate/20161111232717_create_users.rb
+++ db/migrate/20161111232717_create_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users, force: :cascade do |t|
+    create_table :users, force: :cascade, id: false do |t|
       t.string :slack_id, null: false
       t.string :name,     null: false
 
