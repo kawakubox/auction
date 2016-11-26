e1d6f8213a43c6599672205c058ee98f45f15fcd
kawakubox <shigefumi.kawakubo@gmail.com>
2016-11-26 23:55:42 +0900

    Implements POST api/v1/users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class UsersController < ActionController::API
+      def create
+        @user = User.find_or_create_by(slack_id: params[:id]) do |u|
+          u.name = params[:name]
+        end
+        @user.update(name: params[:name]) unless @user.name == params[:name]
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController do
+  describe '#create' do
+    let(:user_params) { { slack_id: 'U0A3DB97E', name: 'kawakubox' } }
+    it do
+      params = user_params.merge(format: :json)
+      post :create, params: params
+      expect(response).to have_http_status :ok
+    end
+
+    it do
+      User.create(user_params)
+      params = user_params.merge(format: :json)
+      post :create, params: params
+      expect(response).to have_http_status :ok
+    end
+  end
+end
